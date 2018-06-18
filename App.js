157216149e012896a573ac2c2afcc860e0e0429e
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-06-18 11:18:21 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,89 +1,82 @@
 import React, { Component } from 'react';
 import './App.css';
-import $ from "jquery";
-import _ from 'lodash';
-
+//Child component
 import MovieCard from "./MovieCard";
 
-//variables needed to create urls.
-var urlArray = []
+
+//Variables needed to create urls.
+var movieArray = []
 var omdb = 'http://www.omdbapi.com/?t='
 var key =  '&apikey=de322a1c'
-var movieArray = [
-  'back+to+the+future',
+//peliculas means movies, in Spanish
+var peliculas = [
+  'march+of+the+penguins',
+  'the+martian',
+  'a+league+of+their+own',
   'indiana+jones',
-  'up',
-  'ghostbusters',
+  'hidden+figures',
+  'tremors',
+  'moana',
+  'maidentrip',
+  'jurassic+park',
+  'zootopia'
 ]
-var releaseDate
 
+//combine website, movie title, and key to create url.
+function newMovie(){
+  for(var i=0; i<peliculas.length; i++){
+    var film = peliculas[i];
+    //store urls in movieArray
+    movieArray.push(omdb + film + key);
+}};
+
+//call the function
+newMovie()
 
 class App extends Component {
 
-  constructor(){
+constructor(){
     super();
-    this.state = {};
+    this.state = {
+      //array to store objects retrieved from API
+      movieInfo: [],
+    };
   }
 
 
-///
-
-//newMovie()
-
-
-//function newMovie(){
-newMovie = () => {
-//turn website, movie title, and key into url.
-//created array of those urls.
-for(var i=0; i<movieArray.length; i++){
-  var film = movieArray[i];
-  urlArray.push(omdb + film + key);
-  //console.log(urlArray);
+//Use APIs to fetch the information.
+componentDidMount() {
+  //loop through the array and fetch info for each URL
+  for (var i = 0; i<=movieArray.length; i++){
+    fetch(movieArray[i])
+    //turn the response into json
+    .then(response=>response.json())
+    .then(response =>{
+       //copy the movieInfo array, add the newest response, then setState
+       let movieCopy = this.state.movieInfo.slice();
+       movieCopy.push(response)
+       this.setState({movieInfo: movieCopy});
+       //console.log(this.state.movieInfo);
+    })  
+  .catch(error => console.log('parsing failed', error)) 
+  }
 }
 
-for (var j=0; j<=urlArray.length; j++){
-     
-  $.ajax({
-    //url: urlString,
-    url: urlArray[j],
-    type: "GET",
-    crossDomain: true,
-    success: function (result) {
-      //var rating = result.Ratings;
-      //console.log(rating);
-      //$("#movieTitle").text(result.Title);
-      var releaseDate = result.Released
-      $("#released").text(result.Released);
-      $("#rated").text(result.Rated);
-      $("#genre").text(result.Genre);
-      $("#website").text(result.Website);
-      $("#awards").text(result.Awards);
-      //$("#ratings").text(Object.entries(rating));
-      this.setState({
-        title: result.Title
-      })
-    }
-  });
- }
-}
-///
 
-
+//use MovieCard child component to render data
   render() {
-    var pelicula = _.map(this.state.title, (movie) => {
-      return <li>{movie.title}</li>;
-    });
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">MOVIE CARDS</h1>
-           <ul>{pelicula}</ul>
         </header>
-
-        <MovieCard title="Back to the Future" image = 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg' getMovie={this.newMovie}/>
-        <MovieCard title="Indiana Jones" image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDJgizEh-6Ggx8OI5L434BTvrBvJbVC243PxaiIXz1HJ7mGxxlJg' getMovie={this.newMovie}/>
-        <MovieCard title="Up" image = 'https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Up_%282009_film%29.jpg/220px-Up_%282009_film%29.jpg' getMovie={this.newMovie}/>
-        <MovieCard title="Ghostbusters" image = 'https://resizing.flixster.com/INKCtDRIQXX3GjXTcVGJ1zBdBR0=/206x305/v1.bTsxMjAxNjcxNTtqOzE3NzM0OzEyMDA7MTUzNjsyMDQ4' getMovie={this.newMovie}/>
+        {this.state.movieInfo.map((movie, index) => (
+            <div>
+              <MovieCard movie = {movie} key = {index}/>
+            </div>
+        )
+        )}
       </div>
     );
   }
